1bb9b4889b1fd5d3946c1a9bf8eafab49cdad8f7
Bobyshev Nikita <Gammanik@gmail.com>
2020-02-25 15:14:10 +0300

    2.3.3: add Fix, Succ, Pred, IsZero

--- hw_2.hs
+++ hw_2.hs
@@ -6,21 +6,26 @@ type Symb = String
 infixl 4 :@:
 infixr 3 :->
 infixl 5 :*
+infixl 4 :+
 
 data Type = Boo
+          | Nat
           | Type :-> Type
           | Type :* Type
+          | Type :+ Type  -- !!
     deriving (Read,Show,Eq)
-
 
 data Pat = PVar Symb
          | PPair Pat Pat
     deriving (Read,Show,Eq)
 
-
 data Term = Fls
           | Tru
           | If Term Term Term
+          | Zero
+          | Succ Term
+          | Pred Term
+          | IsZero Term
           | Idx Int
           | Term :@: Term
           | Lmb Symb Type Term
@@ -28,22 +33,32 @@ data Term = Fls
           | Pair Term Term
           | Fst Term
           | Snd Term
+          | Fix Term
+          | Inl Term Type              -- !!
+          | Inr Type Term              -- !!
+          | Case Term Symb Term Term   -- !!
           deriving (Read,Show)
 
-
 instance Eq Term where
-  Fls       == Fls          =  True
-  Tru       == Tru          =  True
-  If b u w  == If b1 u1 w1  =  b == b1 && u == u1 && w == w1
-  Idx m     == Idx m1       =  m == m1
-  (u:@:w)   == (u1:@:w1)    =  u == u1 && w == w1
-  Lmb _ t u == Lmb _ t1 u1  =  t == t1 && u == u1
-  Let p u w == Let p1 u1 w1 =  p == p1 && u == u1 && w == w1
-  Pair u w  == Pair u1 w1   =  u == u1 && w == w1
-  Fst z     == Fst z1       =  z == z1
-  Snd z     == Snd z1       =  z == z1
-  _         == _            =  False
-
+  Fls       == Fls                 =  True
+  Tru       == Tru                 =  True
+  If b u w  == If b1 u1 w1         =  b == b1 && u == u1 && w == w1
+  Zero      == Zero                =  True
+  Succ u    == Succ u1             =  u == u1
+  Pred u    == Pred u1             =  u == u1
+  IsZero u  == IsZero u1           =  u == u1
+  Idx m     == Idx m1              =  m == m1
+  (u:@:w)   == (u1:@:w1)           =  u == u1 && w == w1
+  Lmb _ t u == Lmb _ t1 u1         =  t == t1 && u == u1
+  Let p u w == Let p1 u1 w1        =  p == p1 && u == u1 && w == w1
+  Pair u w  == Pair u1 w1          =  u == u1 && w == w1
+  Fst z     == Fst z1              =  z == z1
+  Snd z     == Snd z1              =  z == z1
+  Fix u     == Fix u1              =  u == u1
+  Inl u t   == Inl u1 t1           =  u == u1 &&  t == t1           -- !
+  Inr t u   == Inr t1 u1           =  t == t1 && u == u1            -- !
+  Case u _ t s == Case u1 _ t1 s1  =  u == u1 && t == t1 && s == s1 -- !
+  _         == _                   =  False
 
 newtype Env = Env [(Symb,Type)]
   deriving (Read,Show,Eq)
@@ -64,6 +79,11 @@ shift n tg = hlp 0 tg
         hlp v (Pair t1 t2)  = Pair (hlp v t1) (hlp v t2)
         hlp v (Fst t)       = Fst (hlp v t)
         hlp v (Snd t)       = Snd (hlp v t)
+        hlp v (IsZero n)    = IsZero $ hlp v n
+        hlp v (Succ n)      = Succ $ hlp v n
+        hlp v (Pred n)      = Pred $ hlp v n
+        hlp v (Fix n)       = Fix $ hlp v n
+        hlp _ t             = t
 
 
 match :: Pat -> Term -> Maybe [(Symb,Term)]
@@ -90,13 +110,26 @@ substDB j st t = h t
         h (Pair t1 t2)      = Pair (h t1) (h t2)
         h (Fst t)           = Fst (h t)
         h (Snd t)           = Snd (h t)
+        h (IsZero n)       = IsZero $ h n
+        h (Succ n)          = Succ $ h n
+        h (Pred n)         = Pred $ h n
+        h (Fix n)          = Fix $ h n
+        h t                = t
+
+
+
+isNV :: Term -> Bool
+isNV Zero     = True
+isNV (Succ x) = isNV x
+isNV _        = False
 
 isValue :: Term -> Bool
-isValue Fls = True
-isValue Tru = True
+isValue Fls   = True
+isValue Tru   = True
 isValue Lmb{} = True
 isValue (Pair x y) = isValue x && isValue y
-isValue _ = False
+isValue t = isNV t
+
 
 betaRuleDB :: Term -> Term
 betaRuleDB (Lmb s tp t :@: t1) = shift (-1) (substDB 0 (shift 1 t1) t)
@@ -130,6 +163,28 @@ oneStep (Pair t1 t2) | isValue t1 = case (oneStep t2) of
                                   Nothing -> Nothing
                                   Just x -> Just $ Pair t1 x
 oneStep (Pair x y) = flip Pair y <$> oneStep x
+
+
+oneStep (Pred Zero)               = Just Zero
+oneStep (Pred (Succ n)) | isNV n  = Just n
+
+oneStep (Succ t)                  = case (oneStep t) of
+                                  Nothing -> Nothing
+                                  Just x -> Just $ Succ x
+oneStep (Pred t)                  = case (oneStep t) of
+                                  Nothing -> Nothing
+                                  Just x -> Just $ Pred x
+
+oneStep (IsZero Zero)             = Just Tru
+oneStep (IsZero (Succ n)) | isNV n  = Just Fls
+oneStep (IsZero t)                = case (oneStep t) of
+                                  Nothing -> Nothing
+                                  Just x -> Just $ IsZero x
+
+oneStep (Fix l@(Lmb _ _ u)) = Just $ substDB 0 (Fix l) u
+oneStep (Fix t)                = case (oneStep t) of
+                                  Nothing -> Nothing
+                                  Just x -> Just $ Fix x
 oneStep _ = Nothing
 
 
